136e640fd78c85d2b5d0e6b05b59b2e2bc3c0747
Pascal <pascal@pascal.com>
2018-05-04 17:34:44 -0400

    test updates

--- test/test.js
+++ test/test.js
@@ -7,21 +7,25 @@ var path = require("path")
 var assert = require("assert")
 
 // Require all visualizations
-glob.sync("./dist/*.js" ).forEach(function( file ) {
-  require( path.resolve( file ) );
-});
+glob.sync("./dist/*.js").forEach((file) => {
+  require(path.resolve(file))
+})
 
-looker.plugins.visualizations.all().forEach((vis) =>{
+looker.plugins.visualizations.all().forEach((vis) => {
   describe(`${vis.label} (as ${vis.id})`, () => {
-    it("should load and not use unavailable things", () => { assert(true) });
+
+    it("should load and not use unavailable things", () => {
+      // TODO what do we need to assert here
+      assert(true)
+    });
 
     it("should implement create", () => {
-      assert(typeof vis.create === "function");
+      assert(typeof vis.create === "function")
     })
 
     it("should implement update or updateAsync", () => {
-      assert(typeof vis.update === "function" || typeof vis.updateAsync === "function");
+      assert(typeof vis.update === "function" || typeof vis.updateAsync === "function")
     })
 
-  });
-});
+  })
+})
