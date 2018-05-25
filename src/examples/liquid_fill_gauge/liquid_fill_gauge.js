513ad2c8aaf8e6d8c1c1f1270ba81450231340cd
Ian Langworth ☠ <ian@langworth.com>
2018-05-24 20:33:45 -0700

    Fix permissions

--- src/examples/liquid_fill_gauge/liquid_fill_gauge.js
+++ src/examples/liquid_fill_gauge/liquid_fill_gauge.js
@@ -5,6 +5,8 @@
  *
  * Liquid Fill Gauge v1.1
  */
+import * as d3 from 'd3';
+
 export function liquidFillGaugeDefaultSettings() {
   return {
     minValue: 0, // The gauge minimum value.
@@ -41,11 +43,11 @@ export function loadLiquidFillGauge(elementId, value, config) {
 
   var waveHeightScale;
   if (config.waveHeightScaling) {
-    waveHeightScale = d3.scale.linear()
+    waveHeightScale = d3.scaleLinear()
       .range([0, config.waveHeight, 0])
       .domain([0, 50, 100]);
   } else {
-    waveHeightScale = d3.scale.linear()
+    waveHeightScale = d3.scaleLinear()
       .range([config.waveHeight, config.waveHeight])
       .domain([0, 100]);
   }
@@ -80,26 +82,26 @@ export function loadLiquidFillGauge(elementId, value, config) {
   }
 
   // Scales for drawing the outer circle.
-  var gaugeCircleX = d3.scale.linear().range([0, 2 * Math.PI]).domain([0, 1]);
-  var gaugeCircleY = d3.scale.linear().range([0, radius]).domain([0, radius]);
+  var gaugeCircleX = d3.scaleLinear().range([0, 2 * Math.PI]).domain([0, 1]);
+  var gaugeCircleY = d3.scaleLinear().range([0, radius]).domain([0, radius]);
 
   // Scales for controlling the size of the clipping path.
-  var waveScaleX = d3.scale.linear().range([0, waveClipWidth]).domain([0, 1]);
-  var waveScaleY = d3.scale.linear().range([0, waveHeight]).domain([0, 1]);
+  var waveScaleX = d3.scaleLinear().range([0, waveClipWidth]).domain([0, 1]);
+  var waveScaleY = d3.scaleLinear().range([0, waveHeight]).domain([0, 1]);
 
   // Scales for controlling the position of the clipping path.
-  var waveRiseScale = d3.scale.linear()
+  var waveRiseScale = d3.scaleLinear()
     // The clipping area size is the height of the fill circle + the wave height, so we position the clip wave
     // such that the it will overlap the fill circle at all when at 0%, and will totally cover the fill
     // circle at 100%.
     .range([(fillCircleMargin + fillCircleRadius * 2 + waveHeight), (fillCircleMargin - waveHeight)])
     .domain([0, 1]);
-  var waveAnimateScale = d3.scale.linear()
+  var waveAnimateScale = d3.scaleLinear()
     .range([0, waveClipWidth - fillCircleRadius * 2]) // Push the clip area one full wave then snap back.
     .domain([0, 1]);
 
   // Scale for controlling the position of the text within the gauge.
-  var textRiseScaleY = d3.scale.linear()
+  var textRiseScaleY = d3.scaleLinear()
     .range([fillCircleMargin + fillCircleRadius * 2, (fillCircleMargin + textPixels * 0.7)])
     .domain([0, 1]);
 
@@ -108,7 +110,7 @@ export function loadLiquidFillGauge(elementId, value, config) {
     .attr('transform', 'translate(' + locationX + ',' + locationY + ')');
 
   // Draw the outer circle.
-  var gaugeCircleArc = d3.svg.arc()
+  var gaugeCircleArc = d3.arc()
     .startAngle(gaugeCircleX(0))
     .endAngle(gaugeCircleX(1))
     .outerRadius(gaugeCircleY(radius))
@@ -128,7 +130,7 @@ export function loadLiquidFillGauge(elementId, value, config) {
     .attr('transform', 'translate(' + radius + ',' + textRiseScaleY(config.textVertPosition) + ')');
 
   // The clipping wave area.
-  var clipArea = d3.svg.area()
+  var clipArea = d3.area()
     .x(function (d) { return waveScaleX(d.x); })
     .y0(function (d) { return waveScaleY(Math.sin(Math.PI * 2 * config.waveOffset * -1 + Math.PI * 2 * (1 - config.waveCount) + d.y * 2 * Math.PI)); })
     .y1(function (d) { return (fillCircleRadius * 2 + waveHeight); });
@@ -224,18 +226,18 @@ export function loadLiquidFillGauge(elementId, value, config) {
 
       var fillPercent = Math.max(config.minValue, Math.min(config.maxValue, value)) / config.maxValue;
       var waveHeight = fillCircleRadius * waveHeightScale(fillPercent * 100);
-      var waveRiseScale = d3.scale.linear()
+      var waveRiseScale = d3.scaleLinear()
         // The clipping area size is the height of the fill circle + the wave height, so we position the clip wave
         // such that the it will overlap the fill circle at all when at 0%, and will totally cover the fill
         // circle at 100%.
         .range([(fillCircleMargin + fillCircleRadius * 2 + waveHeight), (fillCircleMargin - waveHeight)])
         .domain([0, 1]);
       var newHeight = waveRiseScale(fillPercent);
-      var waveScaleX = d3.scale.linear().range([0, waveClipWidth]).domain([0, 1]);
-      var waveScaleY = d3.scale.linear().range([0, waveHeight]).domain([0, 1]);
+      var waveScaleX = d3.scaleLinear().range([0, waveClipWidth]).domain([0, 1]);
+      var waveScaleY = d3.scaleLinear().range([0, waveHeight]).domain([0, 1]);
       var newClipArea;
       if (config.waveHeightScaling) {
-        newClipArea = d3.svg.area()
+        newClipArea = d3.area()
           .x(function (d) { return waveScaleX(d.x); })
           .y0(function (d) { return waveScaleY(Math.sin(Math.PI * 2 * config.waveOffset * -1 + Math.PI * 2 * (1 - config.waveCount) + d.y * 2 * Math.PI)); })
           .y1(function (d) { return (fillCircleRadius * 2 + waveHeight); });
