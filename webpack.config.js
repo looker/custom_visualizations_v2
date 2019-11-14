f37a769fa758a93e69538530cf9cb4969bab16b9
Daniel Demmel <dain@danieldemmel.me>
2019-11-14 17:03:02 +0000

    ⬆️ Bump package versions

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,21 @@
-var path = require('path')
+const path = require("path")
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin")
 
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-
-var webpackConfig = {
-  mode: 'production',
+const webpackConfig = {
+  mode: "production",
   entry: {
-    advanced_table: './src/examples/advanced_table/advanced_table.js',
-    v1_common: './src/common/common-entry.js',
-    hello_world: './src/examples/hello_world/hello_world.js',
-    hello_world_react: './src/examples/hello_world_react/hello_world_react.js',
-    sankey: './src/examples/sankey/sankey.ts',
-    liquid_fill_gauge: './src/examples/liquid_fill_gauge/liquid_fill_gauge.ts',
-    sunburst: './src/examples/sunburst/sunburst.ts',
-    collapsible_tree: './src/examples/collapsible_tree/collapsible_tree.ts',
-    chord: './src/examples/chord/chord.ts',
-    treemap: './src/examples/treemap/treemap.ts',
-    subtotal: './src/examples/subtotal/subtotal.ts',
-    image_carousel: './src/examples/image_carousel/image_carousel.js',
+    advanced_table: "./src/examples/advanced_table/advanced_table.js",
+    v1_common: "./src/common/common-entry.js",
+    hello_world: "./src/examples/hello_world/hello_world.js",
+    hello_world_react: "./src/examples/hello_world_react/hello_world_react.js",
+    sankey: "./src/examples/sankey/sankey.ts",
+    liquid_fill_gauge: "./src/examples/liquid_fill_gauge/liquid_fill_gauge.ts",
+    sunburst: "./src/examples/sunburst/sunburst.ts",
+    collapsible_tree: "./src/examples/collapsible_tree/collapsible_tree.ts",
+    chord: "./src/examples/chord/chord.ts",
+    treemap: "./src/examples/treemap/treemap.ts",
+    subtotal: "./src/examples/subtotal/subtotal.ts",
+    image_carousel: "./src/examples/image_carousel/image_carousel.js",
     kepler: "./src/examples/kepler/kepler.js"
   },
   output: {
@@ -28,14 +27,14 @@ var webpackConfig = {
   resolve: {
     extensions: [".ts", ".js"]
   },
-  plugins: [
-    new UglifyJSPlugin()
-  ],
+  optimization: {
+    minimizer: [new UglifyJsPlugin()]
+  },
   module: {
     rules: [
-      { test: /\.js$/, loader: "babel-loader" },
-      { test: /\.ts$/, loader: "ts-loader" },
-      { test: /\.css$/, loader: [ 'to-string-loader', 'css-loader' ] }
+      { test: /\.js$/, use: ["babel-loader"] },
+      { test: /\.ts$/, use: ["ts-loader"] },
+      { test: /\.css$/, use: ["to-string-loader", "css-loader"] }
     ]
   },
   stats: {
